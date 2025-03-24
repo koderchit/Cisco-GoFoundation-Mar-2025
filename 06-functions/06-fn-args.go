b97b2fb9fa6c2a35f5375fe2c059bb6cc0231e71
Magesh Kuppan <tkmagesh77@gmail.com>
2025-03-24 14:38:52 +0530

    Higher order functions - functions as argument(s)

---
+++ 06-functions/06-fn-args.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+/*
+func main() {
+	exec("f1") //=> f1 is executed
+	exec("F2") //=> f2 is executed
+}
+
+func exec(fnName string) {
+	// execute a function based on the argument
+	switch fnName {
+	case "f1":
+		f1()
+	case "f2":
+		f2()
+	default:
+		fmt.Println("Invalid argument")
+	}
+}
+*/
+
+func main() {
+	exec(f1)
+	exec(f2)
+}
+
+func exec(fn func()) {
+	fn()
+}
+
+func f1() {
+	fmt.Println("f1 invoked")
+}
+
+func f2() {
+	fmt.Println("f2 invoked")
+}
