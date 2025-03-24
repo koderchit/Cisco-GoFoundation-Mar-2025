9bdb667cd2cb37c9405659264d106f903530f7e0
Magesh Kuppan <tkmagesh77@gmail.com>
2025-03-24 14:53:27 +0530

    problem

---
+++ 06-functions/08-fn-ptr-arg.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var no int = 100
+	fmt.Println("Before incrementing, no =", no)
+	increment(no)
+	fmt.Println("After incrementing, no =", no)
+}
+
+func increment(x int) /* no return values */ {
+	fmt.Println("[increment] address of x :", &x)
+	x += 1
+}
