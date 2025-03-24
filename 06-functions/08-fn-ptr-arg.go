6e733b1bdb03ae12ab1eeb70b18d3a56a0e9b67c
Magesh Kuppan <tkmagesh77@gmail.com>
2025-03-24 14:57:57 +0530

    pointers - solution

--- 06-functions/08-fn-ptr-arg.go
+++ 06-functions/08-fn-ptr-arg.go
@@ -5,11 +5,11 @@ import "fmt"
 func main() {
 	var no int = 100
 	fmt.Println("Before incrementing, no =", no)
-	increment(no)
+	increment(&no)
 	fmt.Println("After incrementing, no =", no)
 }
 
-func increment(x int) /* no return values */ {
+func increment(x *int) /* no return values */ {
 	fmt.Println("[increment] address of x :", &x)
-	x += 1
+	*x += 1
 }
