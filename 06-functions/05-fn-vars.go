982edd6f83f460df708fcdfcde011fe175a7f3a1
Magesh Kuppan <tkmagesh77@gmail.com>
2025-03-24 14:32:01 +0530

    updates

--- 06-functions/05-fn-vars.go
+++ 06-functions/05-fn-vars.go
@@ -16,16 +16,27 @@ func main() {
 		fmt.Printf("Hello %s!\n", name)
 	}
 	sayHello("Magesh")
-	/*
 
+	var greet func(string, string)
+	greet = func(firstName, lastName string) {
+		fmt.Printf("Hi %s %s, Have a nice day!\n", firstName, lastName)
+	}
+	greet("Magesh", "Kuppan")
 
-		func(firstName, lastName string) {
-			fmt.Printf("Hi %s %s, Have a nice day!\n", firstName, lastName)
-		}("Magesh", "Kuppan")
+	var getGreetMsg func(string, string) string
+	getGreetMsg = func(firstName, lastName string) string {
+		return fmt.Sprintf("Hi %s %s, Have a good day!", firstName, lastName)
+	}
+	msg := getGreetMsg("Suresh", "Kannan")
+	fmt.Println(msg)
 
-		msg := func(firstName, lastName string) string {
-			return fmt.Sprintf("Hi %s %s, Have a good day!", firstName, lastName)
-		}("Suresh", "Kannan")
-		fmt.Println(msg)
-	*/
+	var divide func(int, int) (int, int)
+	divide = func(multiplier, divisor int) (int, int) {
+		quotient := multiplier / divisor
+		remainder := multiplier % divisor
+		return quotient, remainder
+	}
+	q, r := divide(100, 7)
+	fmt.Printf("quotient = %d, remainder = %d\n", q, r)
+
 }
